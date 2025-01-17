51af31b0718f576c16bcccef45f4e36e8201b7a0
vehikl-goldsentintel <goldsentintel@vehikl.com>
2025-01-17 16:48:28 -0500

    wip

---
+++ resources/js/Pages/Show.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ user: String })
+</script>
+
+<template>
+  <Layout>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello, {{ user }} welcome to your first Inertia app!</p>
+  </Layout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return 'hi';
-});
+Route::get('/', \App\Http\Controllers\UrlShortenerController::class);
